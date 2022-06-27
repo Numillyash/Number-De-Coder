157da234139616c2bb5a6f9ca8e06c34439bd0ca
Numillyash <ulanovsky.zhora@ya.ru>
2022-06-27 21:49:55 +0300

    Start

---
+++ Coder/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Coder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "ВВЕДИТЕ ДАННЫЕ В ФОРМАТЕ \"4-22-21-4-0-14\"";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox2.Location = new System.Drawing.Point(200, 150);
+            this.textBox2.MaxLength = 100;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "ВВЕДИТЕ КЛЮЧ В ФОРМАТЕ \"ABCDEF\"";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(200, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(400, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Результат";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(200, 300);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(400, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Результат";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "ДеШифратор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Coder/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coder
+{
+    public partial class Form1 : Form
+    {
+        int vigener_f(int a, int b) {
+            return (a + b) % 26;
+        }
+        int vigener_de_f(int c, int b) {
+            if ((c - b) >= 0)
+           	    return c - b;
+            else
+            	return c - b + 26; 
+        }      
+        int a1z26(char value) {
+            for (int i = 0; i < 26; i++)
+            {
+                if (value == symbols[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        string a1z26(string value)
+        {
+            value = value.ToUpper();
+            string result = "";
+            foreach (char ch in value)
+                result += (a1z26(ch)) + " ";
+            return result;
+        }
+        char de_a1z26(int value) {
+            return symbols[value]; 
+        }
+        string encrypt(string _message, string _key) {
+            string message = _message;
+            string key = _key;
+            if (key.Length > message.Length)
+                key = _key.Substring(0, message.Length);
+            else
+                if (key.Length < message.Length) 
+                {
+                    int len_m = _message.Length;
+                    int len_k = _key.Length;
+                    key = "";
+                    while (len_m > len_k)
+                    {
+                        key += _key;
+                        len_m -= len_k;
+                    }
+                    key += _key.Substring(0, len_m);
+                }
+            string result = "";
+            for (int i = 0; i < message.Length; i++)
+                result += vigener_f(a1z26(message[i]), a1z26(key[i])) + "-";//de_a1z26(vigener_f(a1z26(message[i]), a1z26(key[i])));
+            return result;
+        }
+        string decrypt(string _message, string _key) {
+            string message = _message;
+            string[] mas = message.Split();
+            message = "";
+            for (int i = 0; i < mas.Length; i++)
+            {
+                message += de_a1z26(int.Parse(mas[i]));
+            }
+            string key = _key;
+            if (key.Length > message.Length)
+                key = _key.Substring(0, message.Length);
+            else
+            {
+                if (key.Length < message.Length)
+                {
+                    int len_m = message.Length;
+                    int len_k = _key.Length;
+                    key = "";
+                    while (len_m > len_k)
+                    {
+                        key += _key;
+                        len_m -= len_k;
+                    }
+                    key += _key.Substring(0, len_m);
+                }
+            }
+            key = key;
+            string result = "";
+            for (int i = 0; i < message.Length; i++)
+                result += de_a1z26(vigener_de_f(a1z26(message[i]), a1z26(key[i])));
+            return result;
+        }
+
+        private string input_s = "";
+        private string key_s = "";
+        char[] symbols = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            input_s = textBox1.Text;
+            input_s = input_s.ToUpper();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            key_s = textBox2.Text;
+            key_s = key_s.ToUpper();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string res = encrypt(input_s, key_s);
+            textBox3.Text = res;
+        }
+    }
+}
